f7e73e014067d7773f02a5d110a2472d0bf488d1
root <root@sirius.bluemarketics.com>
2023-10-12 14:35:33 +0000

    Premier upload du site PAD & CO

---
+++ wp-content/plugins/tutor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themeum/tutor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '543c6de186a1a0b6cf1104b5d234afac34e13501',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'themeum/tutor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '543c6de186a1a0b6cf1104b5d234afac34e13501',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
